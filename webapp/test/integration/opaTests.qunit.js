1a7deade98bdf48fa6b0a5acff9057a3634f6246
kharbanda21 <sahil.kharbanda17@gmail.com>
2022-05-06 12:06:32 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["yamaha/otc/manageusers/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"yamahaotc/manageusers/test/unit/controller/Home.controller"
+], function () {
+	"use strict";
+});
