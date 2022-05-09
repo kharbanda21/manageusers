fc4fbd99f59ce7be14ebec5cdd0e7ce2eda16104
kharbanda21 <sahil.kharbanda17@gmail.com>
2022-05-09 07:17:46 +0000

    create dialog screen

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,15 +1,41 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/core/Fragment"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,Fragment) {
         "use strict";
 
         return Controller.extend("yamaha.otc.manageusers.controller.Home", {
             onInit: function () {
 
+            },
+
+            onCreate: function () {
+                // var oButton = oEvent.getSource(),
+				var oView = this.getView();
+
+			if (!this._pDialog) {
+				this._pDialog = Fragment.load({
+					id: oView.getId(),
+					name: "yamaha.otc.manageusers.view.create",
+					controller: this
+				}).then(function (oDialog){
+					// this._pDialog = oDialog;
+				    this.getView().addDependent(oDialog);
+                    return oDialog;
+                    // this._pDialog.open();
+				}.bind(this));
+			}
+            // else {
+            //     this._pDialog.open();
+            // }
+
+			this._pDialog.then(function(oDialog){
+				oDialog.open();
+			}.bind(this));
             }
         });
     });
